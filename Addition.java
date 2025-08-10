950f4521474c2aedc8b692de5f3c69f095d02a0b
Gururajkondaguli <91892518+Gururajkondaguli@users.noreply.github.com>
2025-08-10 16:49:46 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition
+{
+	 public static void main(String args[])
+	 {
+	 int x=5;
+	 int y=6;
+	 System.out.println(x+y);
+	 }
+}
---
+++ Gk.java
@@ -0,0 +1,7 @@
+public class Gk
+{
+	 public static void main(String args[])
+	 {
+	 System.out.println("Myself Gururaj");
+	 }
+}
